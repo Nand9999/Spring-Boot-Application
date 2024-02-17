cee731d88054641fed18c9d3315e9732a1f152b5
Nand9999 <nandeeswar386@gmail.com>
2024-02-17 10:06:18 -0600

    initial commit

---
+++ IocodeBookProjects/src/main/java/iocode/web/app/IocodeBookProjects/IocodeBookProjectsApplication.java
@@ -0,0 +1,13 @@
+package iocode.web.app.IocodeBookProjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocodeBookProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IocodeBookProjectsApplication.class, args);
+	}
+
+}
---
+++ IocodeBookProjects/src/test/java/iocode/web/app/IocodeBookProjects/IocodeBookProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package iocode.web.app.IocodeBookProjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocodeBookProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
